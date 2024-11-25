8e185539fed71b1538c1f24b09634dab92ef7129
Carlos Garcia Gomez <carlosgargome@gmail.com>
2024-11-25 19:50:48 +0100

    Cambiando comentarios

--- EjercicioHilos/collab/Semaforo/01_Estacionamiento.py
+++ EjercicioHilos/collab/Semaforo/01_Estacionamiento.py
@@ -7,15 +7,15 @@ semaforo = threading.Semaphore(3)
 
 def acceder_estacionamiento(id_vehiculo):
     print(f"Vehiculo {id_vehiculo} intentando entrar al estacionamiento...")
-    semaforo.acquire()  # Adquirir el semáforo para entrar a la sala
+    semaforo.acquire()  # Adquirir el semáforo para entrar al aparcamiento
     print(f"Vehiculo {id_vehiculo} ha entrado al estacionamiento.")
-    time.sleep(random.uniform(1, 3))  # Simular tiempo en la sala
+    time.sleep(random.uniform(1, 3))  # Simular tiempo en el aparcamiento
     print(f"Vehiculo {id_vehiculo} ha salido del estacionamiento.")
     semaforo.release()  # Liberar el semáforo para permitir que otro entre
 
-# Crear e iniciar varios hilos (personas)
+# Crear e iniciar varios hilos (vehiculos)
 vehiculos = []
-for i in range(10):  # Simulamos 10 personas intentando entrar a la sala
+for i in range(10):  # Simulamos 10 vehiculos intentando entrar al estacionamiento
     t = threading.Thread(target=acceder_estacionamiento, args=(i,))
     vehiculos.append(t)
     t.start()
